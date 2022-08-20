997b561f462aa8fe148b6fff8d3de4c9ba80740a
Soumya Bhandarkar <soumyabhandarkar2000@gmail.com>
2022-08-20 20:27:46 +0530

    resolved few bugs

--- src/App.js
+++ src/App.js
@@ -1,67 +1,28 @@
 import './App.css';
 import React, {useState} from 'react'
-// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import Alert from './components/Alert'; 
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-// } from "react-router-dom";
 
 function App() {
   // It tells the status whether Dark Mode is enabled or not
   const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState('Light mode has been enabled');
-
-  const showAlert = (message, type)=>{
-    setAlert({
-      msg: message,
-      type: type,
-    })
-    setTimeout(()=>{
-      setAlert(null);
-    }, 2000);
-  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils-DarkMode';
-      // setTimeout(()=>{
-      //   document.title = 'TextUtils is Amazing';
-      // }, 2000);
-
-      // setTimeout(()=>{
-      //   document.title = 'Bookmark TextUtils Now';
-      // }, 1500);
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils-LightMode';
     }
   }
   return (
     <>
-      {/* <Router> */}
-        <Navbar title="TxtFormatter" aboutText="AboutTextUtils" mode={mode} toggleMode={toggleMode}/>
-        {/* Here default props will be used as we have not passed any props */}
-        {/* <Navbar /> */}
-        {/* <Alert alert={alert} /> */}
-        <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text here to analyze below" mode={mode}/>
-          {/* <Routes> */}
-            {/* Here, exact keyword is used taaki jab hamara path exact match ho tabhi react hume us link pr jaega otherwise it won't */}
-              {/* <Route exact path="/about" element={<About />} /> */}
-              {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text here to analyze below" mode={mode}/>} /> */}
-            {/* </Routes> */}
-        </div>
-      {/* </Router> */}
+      <Navbar title="TxtFormatter" aboutText="AboutTextUtils" mode={mode} toggleMode={toggleMode}/>
+      <div className="container my-3">
+      <TextForm heading="Try TxtFormatter - Modify text with few clicks !!" mode={mode}/>
+      </div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,76 +2,51 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked: " + text);
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted to Uppercase", "success")
     }
 
     const handleLoClick = ()=>{
-        // console.log("Uppercase was clicked: " + text);
         let newText = text.toLowerCase();
         setText(newText);
-        props.showAlert("Converted to Lowercase", "success")
     }
 
     const handleWordStartUpClick = ()=>{
-        // For Capitalizing the first letter of each word in a string i refered: https://flexiple.com/javascript/javascript-capitalize-first-letter/
-        // console.log("Uppercase was clicked: " + text);
         const arr = text.split(" ");
 
-        //loop through each element of the array and capitalize the first letter.
         for (var i = 0; i < arr.length; i++) {
             arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
         }
 
-        //Join all the elements of the array back into a string 
-        //using a blankspace as a separator 
         const str2 = arr.join(" ");
-        // console.log(str2);
         let newText = str2;
         setText(newText);
     }
 
-    // Harry's video no #11
     const handleCopy = ()=>{
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
     }
 
-    // Harry's video no #11
     const handleExtraSpaces = ()=>{
         let newText = text.split(/[  ]+/);
         setText(newText.join(" "))
     }
 
     const handleClearClick = ()=>{
-        // console.log("Uppercase was clicked: " + text);
         let newText = '';
         setText(newText);
     }
 
-    // Agar, hum React me kabhi bhi kisi event ko agar listen krre hai 
-    // jaise hum yaha onChange ko listen kr rahe hai 
-    // tb yeh hota hai ki jo yeh onChange={handleOnChange} wala function hai
-    // woh toh run hoga he hoga jaise mai koi change karoonga
-    // aur saath he saath hume yaha ek free me object bhi milega
-    // yaha hum us object ko koi bhi naam se skte hai 
-    // as, here we have named it event
-
     const handleOnChange = (event)=>{
-        // console.log("On Change");
         setText(event.target.value);
     }
     const [text, setText] = useState('');
-    // Below, approach will throw an error, as we cannot directly change the state value
-    // text = "add new text";
-    // setText("add new text");
     return (
         <>
         <div className="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
-            <h1>{props.heading}</h1>
+            <h2 className="mb-4">{props.heading}</h2>
             <div className="mb-3">
             <textarea className="form-control" value={text} placeholder="Enter text here" onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#799cd2':'white', color: props.mode === 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
@@ -84,23 +59,12 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
             <h2>Your Text Summary</h2>
-            {/* Here, text.split(" ") yeh hume array dega jisme words honge;
-            and us array ki length will be our no. of words */}
-            {/* Here, .filter function me hume arrow function likhna hai, and it works like that ki jis bhi element k liye true return karega woh element rahega and if returned false toh woh element nhi rahega */}
-            {/* i.e. agar element ki length (0) hai toh woh array me nhi rahega otherwise agar length>0 toh woh array me rahega  */}
-            {/* And our text.length will give us the total characters  */}
-            <p>{text.split(" ").filter((element)=>{
-                return element.length !== 0
-            }).length} words and {text.length} characters</p>
-            {/* Here, in the below code we have taken the time in minutes that on an average 
-            how many words can be read by the normal user in 1 minute which is 125
-            which means to read one word we will require 1/125 = 0.008sec 
-            So, to read the total words written in textarea will be calculated as
-            (time to read a word in seconds * total no. of words) = 0.008 * text.split(" ").length
-            */}
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.split(/\s+/).filter((element)=>{
+                return element.length !== 0}).length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{
+                return element.length !== 0}).length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text.length > 0 ? text : "Enter some text to preview it here"}</p>
+            <p>{text.length > 0 ? text : "Nothing to preview"}</p>
         </div>
         </>
     )
